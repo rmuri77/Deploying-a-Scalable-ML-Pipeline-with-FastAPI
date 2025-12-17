085ae7a5ea283834d6a7779afa7e1072122f7f99
Roberto Murillo <beto.rob77@gmail.com>
2025-12-16 21:36:40 -0800

    Implement model training, inference, and categorical slice metrics

--- ml/model.py
+++ ml/model.py
@@ -1,26 +1,19 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from ml.data import process_data
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
+        """
+        Trains a machine learning model and returns it.
+        """
+        
+        model = LogisticRegression(max_iter=1000)
+        model.fit(X_train, y_train)
+        return model
 
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
-    """
-    # TODO: implement the function
-    pass
 
 
 def compute_model_metrics(y, preds):
@@ -59,9 +52,7 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
-
+    return model.predict(X)
 def save_model(model, path):
     """ Serializes model to a file.
 
@@ -72,14 +63,14 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
+
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
-
+    with open(path, "rb") as f:
+        return pickle.load(f)
 
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
@@ -118,11 +109,16 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    slice_df = data[data[column_name] == slice_value]
+    
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        slice_df,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
