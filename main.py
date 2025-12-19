f2c50804a9b55e5191ced02ca94e843184cdc696
Roberto Murillo <beto.rob77@gmail.com>
2025-12-18 20:21:46 -0800

    Implement FastAPI endpoints and local API interaction

--- main.py
+++ main.py
@@ -26,21 +26,27 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+project_path = os.path.dirname(os.path.abspath(__file__))
+model_dir = os.path.join(project_path, "model")
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+encoder_path = os.path.join(model_dir, "encoder.pkl")
+model_path = os.path.join(model_dir, "model.pkl")
+lb_path = os.path.join(model_dir, "lb.pkl")
+
+encoder = load_model(encoder_path)
+model = load_model(model_path)
+lb = load_model(lb_path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {"message": "Hello from the API!"}
+
 
 
 # TODO: create a POST on a different path that does model inference
@@ -65,10 +71,12 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
+
